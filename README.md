cd35079b9e4758f54074b87c239e8ce3b07117b9
nahid361 <khannahid361@gmail.com>
2024-07-01 22:42:52 +0600

    Binding Attributes

--- README.md
+++ README.md
@@ -25,4 +25,4 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 
 
 learning from code from codeevolution youtube channel 
-#ep - 5 done
+#ep - 6 done
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <h1>ep 5</h1>
+    <h1>ep 7</h1>
     <h2>{{ greet }}, {{ name }}! </h2>
     <p v-html="channel"></p>
     <p v-html="channel2"></p>
     <h2 v-html="channel"></h2>
     <div v-html="hack"></div>
+    <h2 :id="headingId">Binded id</h2>
+    <button :disabled="isDisabled">Disabled</button>
+    <button :disabled="isActive">Active and Pressable</button>
   </div>
 </template>
 
@@ -19,7 +22,10 @@ export default {
       'greet': 'Hello',
       'channel': '<b>CodeEvolution</b>',
       'channel2': 'CodeEvolution',
-      'hack' : `<a href="#" onclick="alert('You have been hacked')">Win A Prize</a>`
+      'hack' : `<a href="#" onclick="alert('You have been hacked')">Win A Prize</a>`,
+      'headingId': 'heading',
+      'isDisabled' : true,
+      'isActive' : false
     }
   },
 }
