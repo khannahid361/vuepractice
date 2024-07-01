7b4cf35fe01a1319b47c6bffc06ff1335a368ae8
nahid361 <khannahid361@gmail.com>
2024-07-02 00:16:27 +0600

    Binding Classes

--- README.md
+++ README.md
@@ -25,4 +25,4 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 
 
 learning from code from codeevolution youtube channel 
-#ep - 6 done
+#ep - 8 done
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>ep 7</h1>
+    <h1>ep 8</h1>
     <h2>{{ greet }}, {{ name }}! </h2>
     <p v-html="channel"></p>
     <p v-html="channel2"></p>
@@ -9,35 +9,60 @@
     <h2 :id="headingId">Binded id</h2>
     <button :disabled="isDisabled">Disabled</button>
     <button :disabled="isActive">Active and Pressable</button>
+    <p :class="show1class && myclass">class is active</p>
+    <p :class="show1class && secondclass">class is disabled</p>
+    <p :class="[pomt,myclass]">Double class</p>
+    <p :class="[show1class ? secondclass : myclass, pomt ]">Conditional class</p>
+    <p :class="{
+      'text-red': isDisabled,
+      'text-green': show2class
+    }">Object Conditional class - note the left side class has to be the css class</p>
   </div>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-  data() {
-    return {
-      'name' : 'Kamrul',
-      'greet': 'Hello',
-      'channel': '<b>CodeEvolution</b>',
-      'channel2': 'CodeEvolution',
-      'hack' : `<a href="#" onclick="alert('You have been hacked')">Win A Prize</a>`,
-      'headingId': 'heading',
-      'isDisabled' : true,
-      'isActive' : false
-    }
-  },
-}
+  export default {
+    name: 'App',
+    data() {
+      return {
+        'name': 'Kamrul',
+        'greet': 'Hello',
+        'channel': '<b>CodeEvolution</b>',
+        'channel2': 'CodeEvolution',
+        'hack': `<a href="#" onclick="alert('You have been hacked')">Win A Prize</a>`,
+        'headingId': 'heading',
+        'isDisabled': true,
+        'isActive': false,
+        'myclass': 'text-red',
+        'secondclass': 'text-green',
+        'show1class': true,
+        'show2class': false,
+        'pomt': 'promoted'
+      }
+    },
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .text-red {
+    color: red;
+  }
+
+  .text-green {
+    color: rgb(29, 6, 235);
+  }
+
+  .promoted {
+    font-style: italic;
+  }
 </style>
